dea2599bd98b8869400024306a5c374cdb78bb69
yuan6636 <aleniverson777@gmail.com>
2026-01-08 18:15:47 +0800

    feat: week2-新增登入與查看產品資訊功能

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,213 @@
-// import { useState } from 'react'
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
+import './assets/style/style.css';
+
+const API_BASE = import.meta.env.VITE_API_BASE;
+const API_PATH = import.meta.env.VITE_API_PATH;
 
 function App() {
-  // const [count, setCount] = useState(0)
+  const [formData, setFormData] = useState({
+    username: '',
+    password: ''
+  });
+
+  const [isAuth, setIsAuth] = useState(false);
+  const [products, setProducts] = useState([]);
+  const [tempProduct, setTempProduct] = useState(null);
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await axios.post(`${API_BASE}/admin/signin`, formData);
+      const { token, expired } = res.data;
+
+      document.cookie = `access_token=${token}; expires=${new Date(expired)};`;
+      axios.defaults.headers.common['Authorization'] = token;
+
+      await getProducts();
+      setIsAuth(true);
+    } 
+    catch (error) {
+      setIsAuth(false);
+      alert(error?.response?.data.message);
+    }
+  };
+
+  const checkLogin = async () => {
+    try {
+      const token = document.cookie
+        .split('; ')
+        .find((row) => row.startsWith('access_token='))
+        .split('=')[1];
+
+      if (token) {
+        axios.defaults.headers.common['Authorization'] = token;
+        await axios.post(`${API_BASE}/api/user/check`);
+        await getProducts();
+        setIsAuth(true);
+      }
+    } 
+    catch (error) {
+      console.error('Token 驗證失敗:', error?.response?.data ?? '請重新登入');
+    }
+  };
+
+  const getProducts = async () => {
+    try {
+      const res = await axios.get(
+        `${API_BASE}/api/${API_PATH}/admin/products`
+      );
+      setProducts(res.data.products);
+    } 
+    catch (error) {
+      console.error('取得產品失敗', error?.response?.data.message);
+    }
+  };
+
+  useEffect(() => {
+    checkLogin();
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
     <>
+      {isAuth ? (
+        <div className="container">
+          <div className="row mt-5">
+            <div className="col-md-6">
+              <h2>產品列表</h2>
+              <table className="table">
+                <thead>
+                  <tr>
+                    <th>產品名稱</th>
+                    <th>原價</th>
+                    <th>售價</th>
+                    <th>是否啟用</th>
+                    <th>查看細節</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {products && products.length > 0 ? (
+                    products.map((item) => (
+                      <tr key={item.id}>
+                        <td>{item.title}</td>
+                        <td>{item.origin_price}</td>
+                        <td>{item.price}</td>
+                        <td>{item.is_enabled ? '啟用' : '未啟用'}</td>
+                        <td>
+                          <button
+                            className="btn btn-primary"
+                            onClick={() => setTempProduct(item)}
+                          >
+                            查看細節
+                          </button>
+                        </td>
+                      </tr>
+                    ))
+                  ) : (
+                    <tr>
+                      <td colSpan="5">尚無產品資料</td>
+                    </tr>
+                  )}
+                </tbody>
+              </table>
+            </div>
+            <div className="col-md-6">
+              <h2>單一產品細節</h2>
+              {tempProduct ? (
+                <div className="card mb-3">
+                  <img
+                    src={tempProduct.imageUrl}
+                    className="card-img-top primary-image"
+                    alt="主圖"
+                  />
+                  <div className="card-body">
+                    <h5 className="card-title">
+                      {tempProduct.title}
+                      <span className="badge bg-primary ms-2">
+                        {tempProduct.category}
+                      </span>
+                    </h5>
+                    <p className="card-text">
+                      商品描述：{tempProduct.description}
+                    </p>
+                    <p className="card-text">商品內容：{tempProduct.content}</p>
+                    <div className="d-flex">
+                      <p className="card-text text-secondary">
+                        <del>{tempProduct.origin_price}</del>
+                      </p>
+                      元 / {tempProduct.price} 元
+                    </div>
+                    <h5 className="mt-3">更多圖片：</h5>
+                    <div className="row row-gap-3">
+                      {tempProduct.imagesUrl?.map((url, index) => (
+                        <div className="col-lg-4 col-md-6" key={index}>
+                          <img src={url} className="images" alt="副圖" />
+                        </div>
+                      ))}
+                    </div>
+                  </div>
+                </div>
+              ) : (
+                <p className="text-secondary">請選擇一個商品查看</p>
+              )}
+            </div>
+          </div>
+        </div>
+      ) : (
+        <div className="container login">
+          <div className="row justify-content-center">
+            <h1 className="h3 mb-3 font-weight-normal">請先登入</h1>
+            <div className="col-8">
+              <form id="form" className="form-signin" onSubmit={handleSubmit}>
+                <div className="form-floating mb-3">
+                  <input
+                    type="email"
+                    className="form-control"
+                    id="username"
+                    placeholder="name@example.com"
+                    name="username"
+                    value={formData.username}
+                    onChange={handleInputChange}
+                    required
+                    autoFocus
+                  />
+                  <label htmlFor="username">Email address</label>
+                </div>
+                <div className="form-floating">
+                  <input
+                    type="password"
+                    className="form-control"
+                    id="password"
+                    placeholder="Password"
+                    name="password"
+                    value={formData.password}
+                    onChange={handleInputChange}
+                    required
+                  />
+                  <label htmlFor="password">Password</label>
+                </div>
+                <button
+                  className="btn btn-lg btn-primary w-100 mt-3"
+                  type="submit"
+                >
+                  登入
+                </button>
+              </form>
+            </div>
+          </div>
+          <p className="mt-5 mb-3 text-muted">&copy; 2024~∞ - 六角學院</p>
+        </div>
+      )}
     </>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
